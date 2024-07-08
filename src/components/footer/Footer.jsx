8aca581e91c582bf21bea03ed498450f96225cdf
Kriti-kaa <137922605+Kriti-kaa@users.noreply.github.com>
2024-07-08 13:37:24 +0530

    footer is added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,92 @@
+import "./footer.css"
+
+function Footer() {
+  return (
+    <div>
+         <footer class="ftco-footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2 logo">About us</h2>
+              <p>Ph.D. Assistance serves as an external mentor to brainstorm your idea and translate that into research model. Hiring a mentor or tutor is common and therefore let your research committee known about the same. We do not offer any writing services without the involvement of the researcher, and therefore we’re merely a translator for non-native English speakers and our services are merely restricted to research scholars to hasten their research process. We offer following services to research scholars across the globe: ideas to translating in writing, editing, proofreading, plagiarism correction, statistical analysis, coding and algorithm, illustration and formatting.</p>
+              <ul class="ftco-footer-social list-unstyled mt-2">
+                <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+         
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Main Services</h2>
+              <ul class="list-unstyled">
+                <li><a href="https://www.phdassistance.com/services/phd-dissertation/" ><span class="fa fa-chevron-right mr-2"></span>PhD Dissertation</a></li>
+                <li><a href="https://www.phdassistance.com/services/phd-coursework/"><span class="fa fa-chevron-right mr-2"></span>Coursework</a></li>
+                <li><a href="https://www.phdassistance.com/services/phd-literature-review/"><span class="fa fa-chevron-right mr-2"></span>Litreature Review</a></li>
+                <li><a href="https://www.phdassistance.com/services/phd-research-methodology/"><span class="fa fa-chevron-right mr-2"></span>PhD Research Methodology</a></li>
+				<li><a href="https://www.phdassistance.com/services/phd-data-analysis/"><span class="fa fa-chevron-right mr-2"></span>Data Analysis</a></li>
+				<li><a href="https://www.phdassistance.com/services/manuscript/"><span class="fa fa-chevron-right mr-2"></span>Manuscript</a></li>
+				<li><a href="https://www.phdassistance.com/services/editing-services/"><span class="fa fa-chevron-right mr-2"></span>Editing Service</a></li>
+				<li><a href="https://www.phdassistance.com/services/animation/"><span class="fa fa-chevron-right mr-2"></span>Animation</a></li>
+				<li><a href="https://www.phdassistance.com/services/courses/"><span class="fa fa-chevron-right mr-2"></span>Courses</a></li>
+
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="https://www.phdassistance.com/blog/"  ><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
+                <li><a href="https://www.phdassistance.com/industries/"><span class="fa fa-chevron-right mr-2"></span>Industries</a></li>
+                <li><a href="https://www.phdassistance.com/pricing/"><span class="fa fa-chevron-right mr-2"></span>Pricing Details</a></li>
+                <li><a href="https://www.phdassistance.com/insights/"><span class="fa fa-chevron-right mr-2"></span>Insights</a></li>
+				<li><a href="https://www.phdassistance.com/help-guide/"><span class="fa fa-chevron-right mr-2"></span>Help-Guide</a></li>
+				<li><a href="https://www.phdassistance.com/sample-work/"><span class="fa fa-chevron-right mr-2"></span>Our Sample Work</a></li>
+				<li><a href="https://www.phdassistance.com/faq/"><span class="fa fa-chevron-right mr-2"></span>FAQ</a></li>
+				<li><a href="https://www.phdassistance.com/career/"><span class="fa fa-chevron-right mr-2"></span>Career</a></li>
+				<li><a href="https://www.phdassistance.com/contact-us/"><span class="fa fa-chevron-right mr-2"></span>Contact us</a></li>
+				<li><a href="https://www.phdassistance.com/terms-and-conditions/"><span class="fa fa-chevron-right mr-2"></span>Terms and conditions</a></li>
+				<li><a href="https://www.phdassistance.com/privacy-policy/"><span class="fa fa-chevron-right mr-2"></span>Privacy policy</a></li>
+				<li><a href="https://www.phdassistance.com/customer-centre/"><span class="fa fa-chevron-right mr-2"></span>Customer Centre</a></li>
+				<li><a href="https://www.phdassistance.com/sitemap.xml"><span class="fa fa-chevron-right mr-2"></span>XML Sitemap</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Contact us </h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="#"></span><span class="text">UK: +44-7537144372</span></li>
+	                <li><a href="#" ><span class="#"></span><span class="text">India: +91-9176966446</span></a></li>
+	                <li><a href="#"><span class="#"></span><span class="text">Landline:    +91-44-42124284( Between 9.00 am - 6.00 pm IST)</span></a></li>
+					<li><span class="#"></span><span class="text">For existing clients: +91-8754446693</span></li>
+					<li><span class="#"></span><span class="text">Customer Care: +91-9384672299</span></li>
+					<li><span class="#"></span><span class="text">Email: info@phdassistance.com</span></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid px-0 py-5 bg-black">
+      	<div class="container">
+      		<div class="row">
+	          <div class="col-md-12">
+		
+	            <p class="mb-0" >
+	   &copy;<script>document.write(new Date().getFullYear());</script> PhDassistance All rights reserved.
+	 </p>
+	          </div>
+	        </div>
+      	</div>
+      </div>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
